8f6ed6dcdf0c8a72714bb86026aaa40ee8c3b841
Master-Daniel <engrdanywiss@gmail.com>
2025-02-22 18:29:25 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,13 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Task Management API')
     .setDescription('API documentation for task management')
